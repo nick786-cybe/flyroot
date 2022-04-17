b6071f0f3c6e736383a99b27d4442acbe961ed2f
Nikhil Rathour <65865069+nick786-cybe@users.noreply.github.com>
2022-04-17 16:07:39 +0530

    Add files via upload

---
+++ blog/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from blog.models import Post
+
+# Register your models here.
+
+
+
+@admin.register(Post)
+
+class PostAdmin(admin.ModelAdmin):
+    class Media:
+        js= ('tinyInject.js',)
